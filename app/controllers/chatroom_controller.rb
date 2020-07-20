6bf2420e477270cc47d00f5206116a9484956806
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-07-20 17:22:29 -0400

    Finish message UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,14 @@
 class ChatroomController < ApplicationController
+  # before_action :require_user
 
   def index
+    @message = Message.new
     @messages = Message.all
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
