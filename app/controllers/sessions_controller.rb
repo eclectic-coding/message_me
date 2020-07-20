c12b2acb9bbc0239455b985293cff97ef8ddb1ec
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-07-20 17:07:15 -0400

    Set up flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = 'You have successfully logged in.'
+      redirect_to root_path
     else
       flash.now[:error] = 'There was something wrong with your login information.'
       render 'new'
